1eaaba8dadab9b98fef9374dfe0e895cf9bbc99a
rajeshwarroy <rajeshwarroy.roni@gmail.com>
2024-03-17 22:06:17 +0600

    1st and 2nd Complete

---
+++ class/class 1st/main.py
@@ -0,0 +1,101 @@
+print('hello world!')       # কোন কিছু প্রিন্ট/হার্ডকোর্ড ডাটা করতে টাইলে এই ফাঙ্কশন তা ইউজ করতে পারি।
+
+# কোর্ড কমেন্ট করা
+print('any')                # this is a inline comment.
+
+# multiline লাইন কমেন্ট
+'''
+def a():
+    return 0
+'''
+
+# veriable ব্যবহার কড়া হয় ডাটা স্টোর করার জন্য।
+a = 6
+# ভ্যারিয়েবল = ডেটা/ডেটা-টাইপ /ভেলু এসাইন করার লাগে।
+
+
+'''
+ভেরিয়েবল লেখার নিয়ম :
+১। কোন স্পেস ব্যবহার করা যাবে না
+২। প্রথম এ কুনো নাম্বার ব্যবহার কড়া যাবে না 
+৩। কোন স্পেশাল কেরেকটার নাম এর মধে ব্যবহার কড়া যাবে, ভেরিয়েবল নাম আর মধে ব্যবহার করা যাবে না (_)
+'''
+
+
+'''
+# case:
+1. Camel Case           === userName
+2. Snake Case           === user_name_all
+3. Pascal Case          === UserNameAll
+'''
+
+
+
+'''
+print function use way:
+1. Normal use
+2. %s string use
+3. f sting
+'''
+
+
+# normal use
+Name = 'Rajeshwar'
+Age = 23
+print('this is the info : ', Name, Age)
+'''      ^ hard code              ^ normal print'''
+
+print(' this is my %s, this is my %s' %(Name, Age))
+'''    ^hard code   ^1st value pass         ^^ এই ভেলু গুলো পাস হবে, আগে নাম, পরে age পাস হবে।'''
+
+print(f'this my name: {Name}, my age: {Age}')
+'''   ^ f string use   ^ name pass     ^ Age pass '''
+
+
+
+'''
+data type:
+1. integer  --      full number(11,44,20,40,55,3)
+2. string   --      all data when inside of ''/"" >>>  '40', "Roni", 'True'
+3. float    --      20.00, 15.32, 55.33
+4. Boolean  --      True / False
+'''
+
+
+
+
+
+
+
+
+
+"""
+
+Class recap........
+
+variable declare rules:
+
+1. space
+2. number at very first
+3. special  character except()
+
+
+print:
+    1. normal
+    2. %s string,   print('my name is %s' %(a))
+    3. f string,    print(f'my name is {a}')
+
+
+variable writing formate:
+1. Camal Case(rajeshwarRoy)
+2. Snake Case(rajeshwar_roy_roni)
+3. Pascle Case(RajeshwarRoy)
+
+
+Data Type:
+1. integre  (full number)
+2. String   (' ' / " ")
+3. float    (Double number)
+4. boolean  (True / False)
+
+"""
